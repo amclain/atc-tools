3a46dd8ec67478693d3208c5310d81c1fe102c3c
Alex McLain <alex@alexmclain.com>
2013-09-26 00:13:04 -0700

    Implemented Aircraft raw, info, code, and equipment.

--- lib/atc-tools/aircraft.rb
+++ lib/atc-tools/aircraft.rb
@@ -1,7 +1,33 @@
 module ATCTools
   class Aircraft
+    # Raw data used to initialize the object,
+    # typically from the VRC client.
+    attr_reader   :raw
+    # Full aircraft info string.
+    attr_accessor :info
+    # Aircraft code. The identifier that would be displayed
+    # on the radar scope (B732 = Boeing 737-200).
+    attr_reader   :code
+    # Aircraft model number.
+    attr_reader   :model
+    # Aircraft equipment code.
+    attr_reader   :equipment
+    
     
     def initialize(aircraft_code = nil, **kvargs)
+      @raw       = aircraft_code || kvargs.fetch(:code, '')
+      @info      = kvargs.fetch :info, ''
+      @model     = kvargs.fetch :model, ''
+      
+      params     = @raw.scan(%r{(?:([a-zA-Z]+)/)?(\w+)(?:/([a-zA-Z]+))?}).flatten
+      
+      @code      = kvargs.fetch(:code, nil) || params[1] || ''
+      @equipment = kvargs.fetch(:equipment, nil) || params[2] || ''
+    end
+    
+    # Returns @raw.
+    def to_s
+      @raw
     end
     
   end
--- test/unit/aircraft_test.rb
+++ test/unit/aircraft_test.rb
@@ -3,8 +3,38 @@ require 'test_helper'
 
 describe ATCTools::Aircraft do
   
-  it "can be extracted from VRC." do
-    skip
+  before do
+    # ------------------------------
+    # TODO: Use a real code from VRC
+    # ------------------------------
+    @aircraft = @object = ATCTools::Aircraft.new 'A321/Q',
+      info: 'A321 - AIRBUS INDUSTRIES (International) A-321 - Engines/Class: 2J/L - C/D Rates: 3500/3500 - SRS Category III'
+  end
+  
+  after do
+    @aircraft = @object = nil
+  end
+  
+  it "exposes the raw data (typically from VRC) used to create the object" do
+    assert_respond_to @aircraft, :raw
+  end
+  
+  it "has a model" do
+    assert_respond_to @aircraft, :model
+  end
+  
+  it "has an aircraft code -- the text displayed on the radar scope" do
+    assert_respond_to @aircraft, :code
+    @aircraft.code.must_equal 'A321'
+  end
+  
+  it "has equipment code" do
+    assert_respond_to @aircraft, :equipment
+    @aircraft.equipment.must_equal 'Q'
+  end
+  
+  it "exposes the full info string from VRC's .acinfo command" do
+    assert_respond_to @aircraft, :info
   end
   
   it "can validate the aircraft's equipment." do
@@ -17,6 +47,10 @@ describe ATCTools::Aircraft do
   
   it "can parse 'A/C Type' field and search cache before pulling data from VRC." do
     skip
+  end
+  
+  it "to_s returns the raw variable" do
+    @aircraft.to_s.must_equal @aircraft.raw
   end
   
 end
