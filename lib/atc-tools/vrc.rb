f56586b72b0f137c27864cf260218489f9e6ef3d
Alex McLain <alex@alexmclain.com>
2013-09-25 20:01:03 -0700

    Created VRC skeleton functions.

--- lib/atc-tools/vrc.rb
+++ lib/atc-tools/vrc.rb
@@ -3,4 +3,18 @@ require 'rautomation'
 # A class for interfacing with the Virtual Radar Client application.
 # http://www1.metacraft.com/VRC/
 class VRC
+  
+  # Extracts the flight plan of the selected aircraft.
+  def extract_flight_plan
+  end
+  
+  # Extracts the callsign of the selected aircraft.
+  # Returns an empty string if no aircraft is selected.
+  def extract_selected_aircraft
+  end
+  
+  # True if an aircraft is selected.
+  def aircraft_selected?
+  end
+  
 end
