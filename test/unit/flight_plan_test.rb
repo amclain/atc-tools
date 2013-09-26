b7e99f958f097d5f0b1876ada9785c89b81a2c9c
Alex McLain <alex@alexmclain.com>
2013-09-25 20:14:30 -0700

    Created FlightPlan instance variables.

--- test/unit/flight_plan_test.rb
+++ test/unit/flight_plan_test.rb
@@ -1,15 +1,54 @@
 require 'atc-tools/flight_plan'
 require 'test_helper'
 
-#  ------------------------------------------------
-#  Can skip VRC automation tests if the application
-#  isn't running.
-#  ------------------------------------------------
-
 describe FlightPlan do
   
-  it "extracts data from the VRC flight plan dialog." do
-    skip
+  before do
+    @fp = @object = FlightPlan.new
+  end
+  
+  after do
+    @fp = @object = nil
+  end
+  
+  it "has an aircraft callsign" do
+    assert_respond_to @fp, :callsign
+  end
+  
+  it "has an aircraft type" do
+    assert_respond_to @fp, :aircraft
+  end
+  
+  it "has flight rules" do
+    assert_respond_to @fp, :rules
+  end
+  
+  it "has a departure airport" do
+    assert_respond_to @fp, :depart
+  end
+  
+  it "has an arrival airport" do
+    assert_respond_to @fp, :arrive
+  end
+  
+  it "has an alternate airport" do
+    assert_respond_to @fp, :alternate
+  end
+  
+  it "has a cruise altitude" do
+    assert_respond_to @fp, :cruise
+  end
+  
+  it "has a squawk code" do
+    assert_respond_to @fp, :squawk
+  end
+  
+  it "has a route" do
+    assert_respond_to @fp, :route
+  end
+  
+  it "has remarks" do
+    assert_respond_to @fp, :remarks
   end
   
   it "can validate flight level based on departure and arrival airport codes" do
@@ -17,10 +56,12 @@ describe FlightPlan do
   end
   
   it "can validate departure procedures" do
+    # This may be a job for a Route class.
     skip
   end
   
   it "can find the name for a VOR" do
+    # This may be a job for a Route class.
     skip
   end
   
